659324ef37ee128ae9c67ff6551fbd8df0f5a0df
rnto4635 <rnto4635@gmail.com>
2024-12-20 18:25:43 +0700

    Admin Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Atribut yang dapat diisi secara massal.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    /**
+     * Relasi ke model User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
